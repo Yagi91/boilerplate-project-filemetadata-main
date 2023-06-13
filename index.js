3980f94c8084c6f26822744e48db95c55a42acba
Yagi91 <bryantimah@gmail.com>
2023-06-14 00:05:55 +0100

    Init basic file structure and apply all the routes and middlewears in index.js

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
 var app = express();
 
+//middleware
+app.use(bodyParser.json());
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// Import routes
+const fineAnalyser = require("./routes/fineAnalyser.routes");
 
+// Routes
+app.use("/api", fineAnalyser);
 
+// // 404 Not Found Middleware
+// app.use((req, res, next) => {
+//   return next({ status: 404, message: "not found" });
+// });
+
+// // Error Handling Middleware
+// app.use((err, req, res, next) => {
+//   let errCode, errMessage;
+//   console.log(err);
+//   if (err.errors) {
+//     // mongoose validation error
+//     errCode = 400; // bad request
+//     const keys = Object.keys(err.errors);
+//     // report the first validation error
+//     errMessage = err.errors[keys[0]].message;
+//   } else {
+//     // generic or custom error
+//     errCode = err.status || 500;
+//     errMessage = err.message || "Internal Server Error";
+//   }
+//   res.status(errCode).json({ error: errMessage });
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
