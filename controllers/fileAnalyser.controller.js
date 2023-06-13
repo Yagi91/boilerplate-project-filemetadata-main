7dcdc32efc6e94d6b5839d18c9e2d88cc2e4100c
Yagi91 <bryantimah@gmail.com>
2023-06-14 00:20:33 +0100

    fileAnalyse controller should log the req into the console when accesing the fileanalyse end point

---
+++ controllers/fileAnalyser.controller.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const fileanalyse = async (req, res) => {
+  console.log(req);
+};
+
+module.exports = {
+  fileanalyse,
+};
--- routes/fileAnalyser.routes.js
+++ routes/fileAnalyser.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const fileAnalyserCtrl = require("../controllers/fileAnalyser.controller");
 
-const router = express.Router();
+const router = express.Router(); // Create router object
 
-router.route("/fileanalyse").post(fileAnalyserCtrl.fileanalyse);
+router.route("/fileanalyse").post(fileAnalyserCtrl.fileanalyse); // Create route for POST request
 
-module.exports = router;
+module.exports = router; // Export router object
