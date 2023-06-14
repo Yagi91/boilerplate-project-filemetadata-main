0c083a173047a62af88423dae92cbe21406bc7b3
Yagi91 <bryantimah@gmail.com>
2023-06-14 01:42:31 +0100

    Manage errors with two middlewears the first to get all unmatched enpoints the second to catch all errors in any of the other routes

--- controllers/fileAnalyser.controller.js
+++ controllers/fileAnalyser.controller.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const fileanalyse = async (req, res) => {
+const fileanalyse = async (req, res, next) => {
   console.log(req.file);
-  res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size,
-  });
+  try {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } catch (err) {
+    // console.log(err);
+    // res.status(500).json({ error: "Server Error" });
+    next(err);
+  }
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -20,28 +20,33 @@ const fineAnalyser = require("./routes/fineAnalyser.routes");
 // Routes
 app.use("/api", fineAnalyser);
 
-// // 404 Not Found Middleware
-// app.use((req, res, next) => {
-//   return next({ status: 404, message: "not found" });
-// });
+// 404 Not Found Middleware
+//This middleware will be executed only if none of the above routes are matched
+app.use((req, res, next) => {
+  return next({ status: 404, message: "not found" }); //next() is used to pass control to the next middleware function
+});
 
-// // Error Handling Middleware
-// app.use((err, req, res, next) => {
-//   let errCode, errMessage;
-//   console.log(err);
-//   if (err.errors) {
-//     // mongoose validation error
-//     errCode = 400; // bad request
-//     const keys = Object.keys(err.errors);
-//     // report the first validation error
-//     errMessage = err.errors[keys[0]].message;
-//   } else {
-//     // generic or custom error
-//     errCode = err.status || 500;
-//     errMessage = err.message || "Internal Server Error";
-//   }
-//   res.status(errCode).json({ error: errMessage });
-// });
+// Error Handling Middleware
+//This middleware will be executed only if any of the above routes throw an error
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+  console.log("In here");
+  console.error(err);
+  if (err.errors) {
+    console.log("first brick");
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || "Internal Server Error";
+    console.log("second brick", errCode, errMessage);
+  }
+  res.status(errCode).json({ error: errMessage });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
